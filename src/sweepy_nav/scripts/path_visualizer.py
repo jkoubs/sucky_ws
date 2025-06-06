0f1ac136f959bb49a810b6e85cbfd0ba9b10bd29
jkoubs <jazkoubi@gmail.com>
2025-06-06 10:10:49 -0700

    change marker color visualizaion

--- src/sweepy_nav/scripts/path_visualizer.py
+++ src/sweepy_nav/scripts/path_visualizer.py
@@ -21,9 +21,9 @@ class PathVisualizer(Node):
         marker.scale.x = 0.3 # Sphere radius
         marker.scale.y = 0.3
         marker.scale.z = 0.3
-        marker.color.r = 1.0
+        marker.color.r = 0.0
         marker.color.g = 0.4
-        marker.color.b = 0.0
+        marker.color.b = 0.2
         marker.color.a = 1.0
         marker.lifetime.sec = 0  # Forever
 
