c311e50f93eebfe4383735c4c6bf728441076017
jkoubs <jazkoubi@gmail.com>
2025-06-06 15:52:52 -0700

    rviz config into mapping launch file

--- src/sweepy_nav/launch/sim_mapping.launch.py
+++ src/sweepy_nav/launch/sim_mapping.launch.py
@@ -6,6 +6,7 @@ import os
 def generate_launch_description():
     sweepy_nav_dir = get_package_share_directory('sweepy_nav')
     params_file = os.path.join(sweepy_nav_dir, 'config', 'mapper_params_online_async.yaml')
+    rviz_config = os.path.join(sweepy_nav_dir, 'rviz', 'mapping.rviz') 
 
     slam_node = Node(
         package='slam_toolbox',
@@ -15,6 +16,16 @@ def generate_launch_description():
         parameters=[params_file]
     )
 
+    # Rviz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config],
+    )
+
     return LaunchDescription([
-        slam_node
+        slam_node,
+        rviz_node
     ])
