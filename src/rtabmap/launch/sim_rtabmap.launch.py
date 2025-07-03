4a47ae4ac7cd60b8211d29acbef4974fb2394303
jkoubs <jazkoubi@gmail.com>
2025-07-02 17:00:27 -0700

    rtabmap setup

---
+++ src/rtabmap/launch/sim_rtabmap.launch.py
@@ -0,0 +1,77 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    localization = LaunchConfiguration('localization')
+
+    parameters = {
+        'frame_id': 'base_link',  # Change if your robot base frame is different
+        'odom_frame_id': 'odom',
+        'subscribe_rgbd': True,
+        'subscribe_scan': True,
+        'approx_sync': True,
+        'sync_queue_size': 10,
+        'Reg/Strategy': '1',  # Use ICP
+        'Reg/Force3DoF': 'true',  # 2D SLAM
+        'Grid/FromDepth': 'false',  # Use 2D LiDAR, not depth for grid map
+        'Icp/MaxCorrespondenceDistance': '0.2',
+        'Icp/PointToPlane': 'false',
+        'Icp/PM': 'false',
+        'RGBD/ProximityBySpace': 'true',
+        'Mem/STMSize': '30',
+    }
+
+    remappings = [
+        ('rgb/image',       '/color/image_raw'),
+        ('depth/image',     '/depth/image_raw'),
+        ('rgb/camera_info', '/color/camera_info'),
+        ('scan',            '/scan_filtered'),
+        ('odom',            '/diffbot_base_controller/odom')
+    ]
+
+    return LaunchDescription([
+        DeclareLaunchArgument('rtabmap_viz',  default_value='true'),
+        DeclareLaunchArgument('rviz',         default_value='true'),
+        DeclareLaunchArgument('localization', default_value='false'),
+        SetParameter(name='use_sim_time', value=True),
+
+        Node(
+            package='rtabmap_sync', executable='rgbd_sync', output='screen',
+            parameters=[parameters,
+                        {'rgb_image_transport': 'compressed',
+                         'depth_image_transport': 'compressedDepth'}],
+            remappings=remappings),
+
+        Node(
+            condition=UnlessCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=['-d']),
+
+        Node(
+            condition=IfCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters,
+                        {'Mem/IncrementalMemory': 'False',
+                         'Mem/InitWMWithAllNodes': 'True'}],
+            remappings=remappings),
+
+        Node(
+            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+            condition=IfCondition(LaunchConfiguration("rtabmap_viz")),
+            parameters=[parameters],
+            remappings=remappings),
+
+        Node(
+            package='rviz2', executable='rviz2', name='rviz2', output='screen',
+            condition=IfCondition(LaunchConfiguration("rviz")),
+        ),
+    ])
